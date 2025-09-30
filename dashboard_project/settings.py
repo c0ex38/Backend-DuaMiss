e58190c21715766f63bba1977523a2c2f870b309
Çağrı Özay <cagriozay@Cagrs-MacBook-Air.local>
2025-09-30 15:13:26 +0300

    Update database connection

--- dashboard_project/settings.py
+++ dashboard_project/settings.py
@@ -76,12 +76,7 @@ DATABASES = {
         "OPTIONS": {
             "sslmode": "require",  # Supabase SSL zorunlu
         },
-    },
-    "sqlite_old": {  # ESKİ SQLite
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",  # dosyan burada
-    },
-    
+    }    
 }
 
 AUTH_PASSWORD_VALIDATORS = [
